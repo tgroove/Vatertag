a33f11e326ecc7798dd729e4c36207b32c125174
tgroove <thorsten.h@gmx.de>
2018-03-29 08:38:39 +0200

    Verbesserungen für den live html Ranglistenexport

--- Vatertag/frmTop10.vb
+++ Vatertag/frmTop10.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+﻿Imports System.IO
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Data.OleDb
@@ -6,16 +6,38 @@ Imports System.Threading
 
 Public Class frmTop10
     Private Sub tmrRangliste_Tick(sender As Object, e As EventArgs) Handles tmrRangliste.Tick
-
+        Const FontSize = 100
         Dim queryString As String
         Static Dim n As Integer
         Dim Erg As String
         Dim LastErg As String = ""
         Dim offset As Integer
         Dim Pos As Integer
-        Dim fieldcount As Integer
+        Dim fieldcount As Integer = 0
+        Dim Spaltentrenner1 As Integer
+        Dim Spaltentrenner2 As Integer
+        Dim FontSizeFactor As Single
         Dim done As Boolean = False
         Dim rtf As String = "{\rtf1\ansi\deff0 {\fonttbl {\f0 Arial;}}"
+
+        Select Case n
+            Case > 20
+                '3 Spalten
+                Spaltentrenner1 = n / 3 + 0.5
+                Spaltentrenner2 = n / 3 * 2 + 0
+                FontSizeFactor = 0.9
+            Case > 10
+                '2 Spalten
+                Spaltentrenner1 = n / 2 + 0.1
+                Spaltentrenner2 = -1
+                FontSizeFactor = 1
+            Case Else
+                '1 Spalte
+                Spaltentrenner1 = -1
+                Spaltentrenner2 = -1
+                FontSizeFactor = 1.1
+        End Select
+
         Dim html As String = "<!DOCTYPE html>" & vbCrLf _
           & "<html lang=""de"">" & vbCrLf _
           & "  <head>" & vbCrLf _
@@ -26,17 +48,17 @@ Public Class frmTop10
           & "      table {" & vbCrLf _
           & "        margin:          0px auto;" & vbCrLf _
           & "        font-family:     'Liberation Sans', Arial, Helvetica, sans-serif;" & vbCrLf _
-          & "        font-size:       70px;" & vbCrLf _
+          & "        font-size:       " & CInt(0.7 * FontSize) & "pt;" & vbCrLf _
           & "        border-collapse: collapse;" & vbCrLf _
           & "      }" & vbCrLf _
           & "      th, td {" & vbCrLf _
           & "        Padding-Top:    5px;" & vbCrLf _
           & "        Padding-Bottom: 5px;" & vbCrLf _
-          & "        Padding-Left:   20px;" & vbCrLf _
-          & "        Padding-Right:  20px;" & vbCrLf _
+          & "        Padding-Left:   11px;" & vbCrLf _
+          & "        Padding-Right:  11px;" & vbCrLf _
           & "        border-spacing: 0px; " & vbCrLf _
           & "        Font-family:    'Liberation Sans', Arial, Helvetica, sans-serif;" & vbCrLf _
-          & "        font-size:      35px;" & vbCrLf _
+          & "        font-size:      " & CInt(0.25 * FontSize * FontSizeFactor) & "pt;" & vbCrLf _
           & "        xText-align:    center;" & vbCrLf _
           & "      }" & vbCrLf _
           & "      td {" & vbCrLf _
@@ -46,11 +68,13 @@ Public Class frmTop10
           & "    </style>" & vbCrLf _
           & "  </head>" & vbCrLf _
           & "  <body>" & vbCrLf _
-          & "    <font face=""verdana"">" & vbCrLf _
+          & "    <font face=""Helvetica"">" & vbCrLf _
+          & "      <p><strong><div style=""font-size:" & CInt(0.6 * FontSize) & "pt;text-align:center"">Aktuelle Rangliste</div></strong></p>" & vbCrLf _
           & "      <table  border=""5"" bordercolor=WHITE>" & vbCrLf _
-          & "        <caption><p><strong>Aktuelle Rangliste</strong></p></caption>" & vbCrLf _
           & "        <tr><td valign=""top"">" & vbCrLf _
           & "          <table>"
+
+        '& "        <caption><strong><span style=""font-size:" & CInt(0.7 * FontSize) & "pt"">&nbsp;</span>Aktuelle Rangliste<span style=""font-size:" & CInt(0.7 * FontSize) & "pt"">&nbsp;</span></strong></caption>" & vbCrLf _
 
         queryString = "SELECT ID, Name, Platzierung, Scheibe1, Scheibe2, Scheibe3, Scheibe4 " _
             & " FROM Kunden " _
@@ -98,19 +122,21 @@ Public Class frmTop10
                     html = html & vbCrLf _
                       & "            <tr>" _
                       & "<td align=""center"">" & Pos & " </td>" _
-                      & "<td align=""left"">" & dataReader(1).ToString & " </td>" _
-                      & "<td>&nbsp;&nbsp;</td>" _
+                      & "<td align=""left"">" & dataReader(1).ToString & "<span style=""font-size:" & CInt(0.2 * FontSize * FontSizeFactor) & "pt"">&nbsp;(" & dataReader(0) & ")</span></td>" _
+                      & "<td>&nbsp;</td>" _
                       & "<td align=""center"">" & dataReader(3).ToString & " </td>" _
                       & "<td align=""center"">" & dataReader(4).ToString & " </td>" _
                       & "<td align=""center"">" & dataReader(5).ToString & " </td>" _
                       & "<td align=""center"">" & dataReader(6).ToString & " </td>" _
                       & " </tr>"
-                    If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
+                    'If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
+                    If (n = Spaltentrenner1) Or (n = Spaltentrenner2) Then
                         html = html & vbCrLf _
-                          & "          </table>" & vbCrLf _
-                          & "        </td>" & vbCrLf _
-                          & "        <td valign=""top"">" & vbCrLf _
-                          & "          <table>"
+                              & "          </table>" & vbCrLf _
+                              & "        </td>" & vbCrLf _
+                              & "        <td>&nbsp;</td>" & vbCrLf _
+                              & "        <td valign=""top"">" & vbCrLf _
+                              & "          <table>"
                     End If
                 Loop
                 dataReader.Close()
@@ -138,8 +164,7 @@ Public Class frmTop10
           & "</html>"
         tmrRangliste.Enabled = True
         'Debug.Print(html)
-        My.Computer.FileSystem.WriteAllText(Replace(My.Settings.lastFile, ".accdb", ".html", 1,, CompareMethod.Text), html, False)
-
+        My.Computer.FileSystem.WriteAllText(Path.GetDirectoryName(My.Settings.lastFile) & "\rangliste.html", html, False)
     End Sub
 
 
