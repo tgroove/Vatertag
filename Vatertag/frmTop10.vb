906019e6a026c074eebec94e9b5b67be1fb9b979
tgroove <thorsten.h@gmx.de>
2018-03-31 21:43:20 +0200

    Jetzt nur noch zwei Spalten

--- Vatertag/frmTop10.vb
+++ Vatertag/frmTop10.vb
@@ -53,10 +53,17 @@ Public Class frmTop10
                 FontSizeFactor = 1
         End Select
 
+        Dim ZeilenProSpalte = 15
+        FontSizeFactor = 1.1
+        border = 3
+        Padding = 8
+
         Dim html As String
         Dim html_page1 As String = ""
         Dim html_page2 As String = ""
-        Static page As Integer = 1
+        Dim html_page(10) As String
+        Dim page As Integer = 0
+        Static p As Integer = 0
         Dim html_vor As String = "<!DOCTYPE html>" & vbCrLf _
           & "<html lang=""de"">" & vbCrLf _
           & "  <head>" & vbCrLf _
@@ -93,7 +100,14 @@ Public Class frmTop10
           & "      <table  border=""5"" bordercolor=WHITE>" & vbCrLf _
           & "        <tr>" & vbCrLf _
           & "          <td valign=""top"">" & vbCrLf _
-          & "            <table>"
+          & "            <table>" & vbCrLf _
+          & "              <col width=""90"">" & vbCrLf _
+          & "              <col width=""600"">" & vbCrLf _
+          & "              <col width=""60"">" & vbCrLf _
+          & "              <col width=""60"">" & vbCrLf _
+          & "              <col width=""60"">" & vbCrLf _
+          & "              <col width=""60"">" & vbCrLf _
+          & "              <col width=""60"">" & vbCrLf _
 
         '& "        <caption><strong><span style=""font-size:" & CInt(0.7 * FontSize) & "pt"">&nbsp;</span>Aktuelle Rangliste<span style=""font-size:" & CInt(0.7 * FontSize) & "pt"">&nbsp;</span></strong></caption>" & vbCrLf _
 
@@ -140,8 +154,9 @@ Public Class frmTop10
                     rtf = rtf & " " & Pos & "  " & dataReader(1).ToString _
                       & "\tqr\tx4320\tab " _
                       & Erg & " \par\pard "
-                    If n <= 3 * Spaltentrenner1 Then
-                        html_page1 = html_page1 & vbCrLf _
+                    'If n <= 2 * ZeilenProSpalte Then
+                    If (n Mod (2 * ZeilenProSpalte)) = 1 Then page = page + 1
+                    html_page(page) = html_page(page) & vbCrLf _
                           & "              <tr>" _
                           & "<td align=""center"">" & Pos & " </td>" _
                           & "<td align=""left"">" & dataReader(1).ToString & "<span style=""font-size:" & CInt(0.2 * FontSize * FontSizeFactor) & "pt"">&nbsp;(" & dataReader(0) & ")</span></td>" _
@@ -150,36 +165,46 @@ Public Class frmTop10
                           & "<td align=""center"">" & dataReader(5).ToString & " </td>" _
                           & "<td align=""center"">" & dataReader(6).ToString & " </td>" _
                           & " </tr>"
-                        'If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
-                        If (n = Spaltentrenner1) Or (n = Spaltentrenner2) Then
-                            html_page1 = html_page1 & vbCrLf _
+                    'If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
+                    'If (n = Spaltentrenner1) Or (n = Spaltentrenner2) Then
+                    If (n Mod (2 * ZeilenProSpalte)) = ZeilenProSpalte Then
+                        html_page(page) = html_page(page) & vbCrLf _
                               & "            </table>" & vbCrLf _
                               & "          </td>" & vbCrLf _
-                              & "          <td>&nbsp;</td>" & vbCrLf _
+                              & "          <td>&nbsp;&nbsp;&nbsp;</td>" & vbCrLf _
                               & "          <td valign=""top"">" & vbCrLf _
-                              & "            <table>"
-                        End If
-                    Else
-                        html_page2 = html_page2 & vbCrLf _
-                          & "              <tr>" _
-                          & "<td align=""center"">" & Pos & " </td>" _
-                          & "<td align=""left"">" & dataReader(1).ToString & "<span style=""font-size:" & CInt(0.2 * FontSize * FontSizeFactor) & "pt"">&nbsp;(" & dataReader(0) & ")</span></td>" _
-                          & "<td align=""center"">" & dataReader(3).ToString & " </td>" _
-                          & "<td align=""center"">" & dataReader(4).ToString & " </td>" _
-                          & "<td align=""center"">" & dataReader(5).ToString & " </td>" _
-                          & "<td align=""center"">" & dataReader(6).ToString & " </td>" _
-                          & " </tr>"
-                        'If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
-                        If (n = Spaltentrenner1 + 3 * Spaltentrenner1) Or (n = Spaltentrenner2 + 3 * Spaltentrenner1) Then
-                            html_page2 = html_page2 & vbCrLf _
-                              & "            </table>" & vbCrLf _
-                              & "          </td>" & vbCrLf _
-                              & "          <td>&nbsp;</td>" & vbCrLf _
-                              & "          <td valign=""top"">" & vbCrLf _
-                              & "            <table>"
-                        End If
+                              & "            <table>" & vbCrLf _
+                              & "              <col width=""90"">" & vbCrLf _
+                              & "              <col width=""600"">" & vbCrLf _
+                              & "              <col width=""60"">" & vbCrLf _
+                              & "              <col width=""60"">" & vbCrLf _
+                              & "              <col width=""60"">" & vbCrLf _
+                              & "              <col width=""60"">" & vbCrLf _
+                              & "              <col width=""60"">" & vbCrLf
 
                     End If
+                    'Else
+                    'html_page2 = html_page2 & vbCrLf _
+                    '      & "              <tr>" _
+                    '      & "<td align=""center"">" & Pos & " </td>" _
+                    '      & "<td align=""left"">" & dataReader(1).ToString & "<span style=""font-size:" & CInt(0.2 * FontSize * FontSizeFactor) & "pt"">&nbsp;(" & dataReader(0) & ")</span></td>" _
+                    '      & "<td align=""center"">" & dataReader(3).ToString & " </td>" _
+                    '      & "<td align=""center"">" & dataReader(4).ToString & " </td>" _
+                    '      & "<td align=""center"">" & dataReader(5).ToString & " </td>" _
+                    '      & "<td align=""center"">" & dataReader(6).ToString & " </td>" _
+                    '      & " </tr>"
+                    '    'If (fieldcount > 4) And (n = Int(fieldcount / 2 + 0.5)) Then
+                    '    'If (n = Spaltentrenner1 + 3 * Spaltentrenner1) Or (n = Spaltentrenner2 + 3 * Spaltentrenner1) Then
+                    '    If (n Mod ZeilenProSpalte) = 0 Then
+                    '        html_page2 = html_page2 & vbCrLf _
+                    '          & "            </table>" & vbCrLf _
+                    '          & "          </td>" & vbCrLf _
+                    '          & "          <td>&nbsp;</td>" & vbCrLf _
+                    '          & "          <td valign=""top"">" & vbCrLf _
+                    '          & "            <table>"
+                    '    End If
+
+                    'End If
                 Loop
                 dataReader.Close()
                 connection.Close()
@@ -208,18 +233,20 @@ Public Class frmTop10
         tmrRangliste.Enabled = True
         'Debug.Print(html)
 
-        If html_page2 <> "" Then
-            page = page + 1
-            If page = 10000 Then page = 1
-            If (page Mod 10) < 7 Then
-                html = html_vor & html_page1 & html_nach
-            Else
-                html = html_vor & html_page2 & html_nach
-            End If
-        Else
-            html = html_vor & html_page1 & html_nach
-        End If
-
+        'If page > 1 Then
+        '    page = page + 1
+        '    If page = 10000 Then page = 1
+        '    If (page Mod 10) < 7 Then
+        '        html = html_vor & html_page1 & html_nach
+        '    Else
+        '        html = html_vor & html_page2 & html_nach
+        '    End If
+        'Else
+        '    html = html_vor & html_page1 & html_nach
+        'End If
+        p = p + 1
+        If p > 10000 Then p = 0
+        html = html_vor & html_page(((p \ 4) Mod (page)) + 1) & html_nach
         If mode = "Auswertung" Then
             My.Computer.FileSystem.WriteAllText(Path.GetDirectoryName(My.Settings.lastFile) & "\rl.htm", html, False)
         Else
